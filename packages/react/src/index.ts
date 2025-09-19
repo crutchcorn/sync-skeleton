06e122cad1f5aab674a6c9e4dd2960649a9f759c
Corbin Crutchley <git@crutchcorn.dev>
2025-09-19 06:47:23 -0700

    fix: remove unused paint blocking in React adapter

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,13 +1,10 @@
-import { useEffect, useLayoutEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getSkeletonObserver } from '@sync-skeleton/core'
-
-const useIsomorphicLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect
 
 export const useSkeleton = () => {
   const [ref, setRef] = useState<HTMLElement | null>(null)
 
-  useIsomorphicLayoutEffect(() => {
+  useEffect(() => {
     return getSkeletonObserver(ref)
   }, [ref])
 
